9b4c1aef9cac10e07aa44eb047dcaea7426ef8d3
LeeYulhee <reasonoflife39@gmail.com>
2024-09-05 09:52:09 +0900

    12강 : 서블릿 컨테이너 띄우기

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,12 +1,21 @@
 package tobyspring.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
-    }
 
+        // TomcatServletWebServerFactory : 이 자체가 톰캣 서블릿 웹서버는 아니고, 톰캣 서블릿 웹서버 생성 과정, 설정 지원 클래스
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // getWebServer : 서블릿 컨테이너를 만드는 생성 함수
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+
+    }
 }
