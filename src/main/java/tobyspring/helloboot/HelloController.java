8e9f2aa6b3ee5a5eb4e20bb940654aa10baa4730
LeeYulhee <reasonoflife39@gmail.com>
2024-09-12 09:13:00 +0900

    19강 : 의존 오브젝트 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,9 +3,14 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        // Controller의 중요한 역할 중 하나는 유저의 요청 사항을 검증하는 것 : 파라미터가 이상하거나 Null이면 Exception을 발생시키는 등
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
