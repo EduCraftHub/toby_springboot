47ded99c0fa2b3403c45ef6c12f771b337446d89
LeeYulhee <reasonoflife39@gmail.com>
2024-09-24 09:38:35 +0900

    23강 : 애노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,10 +1,13 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -13,8 +16,11 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    // GetMapping 등이 붙어 있으면 웹 요청을 처리할 수 있도록 만들어진 컨트롤러라고 판단 후 그 안의 요청 정보 추출해서 매핑 테이블을 만들어둠
+    @GetMapping()
+    @ResponseBody // 이 어노테이션을 이용하면 웹 응답의 바디에 넣어서 전달 할 수 있음 => @RestController가 붙으면 모든 메서드에 @RequestBody가 붙었다고 가정
     public String hello(String name) {
-        // Controller의 중요한 역할 중 하나는 유저의 요청 사항을 검증하는 것 : 파라미터가 이상하거나 Null이면 Exception을 발생시키는 등
+    // String을 return 하면 해당 이름을 가진 뷰를 찾아서 return
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
