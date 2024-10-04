98144e11b1da58eef06e05be5751502361e3ef19
LeeYulhee <reasonoflife39@gmail.com>
2024-10-04 17:25:54 +0900

    38강 : 자동 구성 애노테이션 적용

--- src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
+// proxyBeanMethods : @Configuration을 이용해 애플리케이션 안에 우리가 원하는 빈을 등록하는 경우가 있음
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {
 }
