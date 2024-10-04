89bddba7f0fcdf018a19dce6757eca54b4b0aca0
LeeYulhee <reasonoflife39@gmail.com>
2024-10-04 19:39:29 +0900

    41강 : @Conditional과 Condition

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,28 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     // Bean은 ID가 하나씩 지정됨(기본적으로 메서드 이름을 따라감)
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletContainer() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
