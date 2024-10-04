6c56f29828a90e45e073e9019ab5443976e6253a
LeeYulhee <reasonoflife39@gmail.com>
2024-10-04 17:04:06 +0900

    37강 : 자동 구성 정보 파일 분리

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,32 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        // load에 애너테이션의 클래스 오브젝트를 넣음
+        // classLoader : 어떤 리소스를 읽어 올 때는 classLoader를 사용
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
+
+        /* Stream을 사용하는 방법
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+         */
     }
 }
