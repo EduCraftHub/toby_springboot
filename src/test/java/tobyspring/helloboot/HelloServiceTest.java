9b4f64adc831627ff1460853818e97f3b5ba53eb
LeeYulhee <reasonoflife39@gmail.com>
2024-10-04 12:45:49 +0900

    32강 : 메타 애노테이션과 합성 애노테이션

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,9 +3,22 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+// ElementType.ANNOTATION_TYPE를 설정해줘야 다른 어노테이션의 메타 어노테이션으로 사용 가능
+// Composed Annotation(합성 어노테이션)은 메타 어노테이션을 1개 이상 갖고 있는 어노테이션
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest{
+}
+
 public class HelloServiceTest {
 
-    @Test
+    @UnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
