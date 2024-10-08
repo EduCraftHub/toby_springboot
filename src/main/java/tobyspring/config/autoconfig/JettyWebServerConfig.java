bda30c2add0365bf84209eec32e47466342fdf61
LeeYulhee <reasonoflife39@gmail.com>
2024-10-08 09:53:29 +0900

    43강 : 커스텀 @Conditional - 2

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -9,22 +9,15 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     // Bean은 ID가 하나씩 지정됨(기본적으로 메서드 이름을 따라감)
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletContainer() {
         return new JettyServletWebServerFactory();
-    }
-
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return ClassUtils.isPresent("org.eclipse.jetty.server.Server",
-                    context.getClassLoader());
-        }
     }
 }
--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -8,23 +8,14 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletContainer() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // 톱캣 클래스(라이브러리)가 현재 프로젝트에 포함 되어 있으면 true를 return
-            // 클래스 로더를 이전에는 생성자나 setter 메서드로 주입을 받았는데, Condition을 구현한 메서드의 ConditionContext에서 애플리케이션 환경에 관한 오브젝트를 받아올 수 있음
-            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
-                    context.getClassLoader());
-        }
     }
 }
