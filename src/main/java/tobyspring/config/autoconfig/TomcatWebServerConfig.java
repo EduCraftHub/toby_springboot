2caece41144c02dc342709ae51e33dfc4a622f7c
LeeYulhee <reasonoflife39@gmail.com>
2024-10-08 09:38:03 +0900

    43강 : 커스텀 @Conditional - 1

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
@@ -20,7 +21,10 @@ public class TomcatWebServerConfig {
     static class TomcatCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            // 톱캣 클래스(라이브러리)가 현재 프로젝트에 포함 되어 있으면 true를 return
+            // 클래스 로더를 이전에는 생성자나 setter 메서드로 주입을 받았는데, Condition을 구현한 메서드의 ConditionContext에서 애플리케이션 환경에 관한 오브젝트를 받아올 수 있음
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+                    context.getClassLoader());
         }
     }
 }
