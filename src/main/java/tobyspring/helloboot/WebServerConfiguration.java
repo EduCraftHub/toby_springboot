0180f4f89f22923e9e514235b9ef698ae8eebcdb
LeeYulhee <reasonoflife39@gmail.com>
2024-10-10 09:35:41 +0900

    47강 : 자동 구성에 Environment 프로퍼티 적용

--- src/main/java/tobyspring/helloboot/WebServerConfiguration.java
+++ src/main/java/tobyspring/helloboot/WebServerConfiguration.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+/*
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 public class WebServerConfiguration {
-    // 자동 구성 ㅈ어보 대신 사용자가 직접 정의한 구성 정보 사용하기 -> 유저 구성 정보가 있다면 자동 구성 정보보다 우선 적용 되게
+    // 자동 구성 정보 대신 사용자가 직접 정의한 구성 정보 사용하기 -> 유저 구성 정보가 있다면 자동 구성 정보보다 우선 적용 되게
     @Bean
     ServletWebServerFactory customerWebServerFactory() {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
@@ -15,3 +16,4 @@ public class WebServerConfiguration {
         return serverFactory;
     }
 }
+ */
