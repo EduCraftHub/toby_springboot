3d7cd1bb53fbaece22f9c9e8ff4945a29ab5c726
LeeYulhee <reasonoflife39@gmail.com>
2024-10-15 12:14:55 +0900

    50강 : 프로퍼티 빈의 후처리 도입

--- src/main/java/tobyspring/config/autoconfig/ServerProperties.java
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -1,7 +1,11 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import tobyspring.config.MyConfigurationProperties;
 
+@MyConfigurationProperties(prefix = "server")
+// BeanPostProcessor : 스프링에는 스프링 컨테이너의 기능을 확장할 수 있는 포인트가 있는데, 이건 Bean 오브젝트를 만든 다음 그걸 가공할 수 있는 기회를 주는 것
+// => ServerProperties 클래스의 오브젝트가 만들어지면 property를 바인딩 하는 작업을 할 예정
 public class ServerProperties {
 
     private String  contextPath;
--- src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
+++ src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
@@ -1,3 +1,4 @@
+/* 미사용
 package tobyspring.config.autoconfig;
 
 import org.springframework.boot.context.properties.bind.Binder;
@@ -10,17 +11,18 @@ public class ServerPropertiesConfig {
 
     @Bean
     public ServerProperties serverProperties(Environment environment) {
-        /*
+        // 이전 코드
         ServerProperties properties = new ServerProperties();
 
         properties.setContextPath(environment.getProperty("contextPath"));
         properties.setPort(Integer.parseInt(environment.getProperty("port")));
 
         return properties;
-         */
+
 
         // ServerProperties로 바인딩 하겠다는 듯
         // 프로퍼티스의 프로퍼티 이름과 클래스의 프로퍼티 이름(Getter, Setter로 되어 있는)을 자동으로 바인딩
         return Binder.get(environment).bind("", ServerProperties.class).get();
     }
 }
+ */
