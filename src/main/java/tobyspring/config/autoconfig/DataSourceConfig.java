b3c0f516df7a326f8e57d76f605e72e5a2688ea0
LeeYulhee <reasonoflife39@gmail.com>
2024-10-16 12:52:42 +0900

    52강 : DataSource 자동 구성 클래스

---
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,67 @@
+package tobyspring.config.autoconfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
+import tobyspring.config.MyAutoConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+// JdbcOperations가 있을 때만 해당 클래스의 빈을 생성
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+// 위의 Conditional 조건이 충족되어 사용이 될 때만 이 프로퍼티 파일이 빈으로 등록 되도록 지정
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+
+    @Bean
+    // 해당 클래스가 있으면 빈 생성
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    @ConditionalOnMissingBean
+    DataSource hikariDataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    // 앞에서 빈이 생성되지 않았다면 이 빈 생성
+    @ConditionalOnMissingBean
+    DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        // 커넥션 풀이 없어서 매번 새로운 커넥션을 만들기 때문에 운영 시스템에서는 사용 하면 안 됨
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        // 프로퍼티 값에서 커넥션 관련 정보를 받아 사용
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
+    }
+}
