e75970aae56a6efb993eafaa6d50936888f631ad
LeeYulhee <reasonoflife39@gmail.com>
2024-10-23 10:23:27 +0900

    55강 : 리포지토리를 사용하는 HelloService

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,27 @@
 package tobyspring.helloboot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import tobyspring.config.EnableMyAutoConfiguration;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellobootApplication {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HellobootApplication(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // PostConstruct : 자바 표준 메서드로 원래 스프링 프레임 워크에 있던 라이프 사이클 인터페이스 방식을 간결하게 대체 할 수 있음
+    @PostConstruct
+    void init() {
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
 
     /*
     ApplicationRunner를 구현한 Bean을 등록하면, 스프링부트 초기화 작업 완료 후에 해당 인터페이스에 정의된 run 메서드를 통해 실행
--- src/main/java/tobyspring/helloboot/SimpleHelloService.java
+++ src/main/java/tobyspring/helloboot/SimpleHelloService.java
@@ -6,8 +6,21 @@ import org.springframework.stereotype.Service;
 // @Component : Component 스캔은 @Component이 붙은 클래스를 찾아서 빈으로 등록 해줌
 @Service
 public class SimpleHelloService implements HelloService {
+    private final HelloRepository helloRepository;
+
+    public SimpleHelloService(HelloRepository helloRepository) {
+        this.helloRepository = helloRepository;
+    }
+
     @Override
     public String sayHello(String name) {
+        this.helloRepository.increaseCount(name);
+
         return "Hello " + name;
+    }
+
+    @Override
+    public int countOf(String name) {
+        return this.helloRepository.countOf(name);
     }
 }
